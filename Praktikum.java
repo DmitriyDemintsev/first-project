6afe034305ff1476fad38c050b38bcb4f66600c1
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2023-05-25 11:34:23 +0300

    Объявлен метод main, заполнены поля

--- Praktikum.java
+++ Praktikum.java
@@ -1,3 +1,11 @@
 public class Praktikum {
+	public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Сколько денег у вас осталось до зарплаты?");
+        double moneyBeforeSalary = scanner.nextDouble();
+        System.out.println("Сколько дней до зарплаты?");
+        int daysBeforeSalary = scanner.nextInt();
+    }
 
 }
